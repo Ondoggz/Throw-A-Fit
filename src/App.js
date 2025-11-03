d717b703eba88a6c80e780dda818b19de13e531d
B yss <20240031277@my.xu.edu.ph>
2025-11-03 15:33:53 +0800

    uhh homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./categories/home";
+import Upload from "./categories/upload";
 import Closet from "./categories/closet";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Default route "/" shows Home */}
         <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
         <Route path="/closet" element={<Closet />} />
       </Routes>
     </Router>
